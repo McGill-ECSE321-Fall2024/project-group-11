ba8de3b64d38102903e31173bb74f03ca035088b
JeffreyNahas <jeffrey.nahas@mail.mcgill.ca>
2024-10-06 13:37:54 -0400

    Initial Commit

---
+++ VideoGamesSystem-Backend/src/main/java/ca/mcgill/_1/videogamessystem/VideogamessystemApplication.java
@@ -0,0 +1,13 @@
+package ca.mcgill._1.videogamessystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VideogamessystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VideogamessystemApplication.class, args);
+	}
+
+}
---
+++ VideoGamesSystem-Backend/src/test/java/ca/mcgill/_1/videogamessystem/VideogamessystemApplicationTests.java
@@ -0,0 +1,13 @@
+package ca.mcgill._1.videogamessystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VideogamessystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
