52bd2655c4775517e04f7548c0a3d62c40d2e24c
RamiNawam <rami.elnawam@mail.mcgill.ca>
2024-10-12 11:58:58 -0400

    updated model

---
+++ VideoGamesSystem-Backend/src/main/java/ca/mcgill/_1/videogamessystem/model/Owner.java
@@ -0,0 +1,33 @@
+/*PLEASE DO NOT EDIT THIS CODE*/
+/*This code was generated using the UMPLE 1.35.0.7523.c616a4dce modeling language!*/
+
+
+
+// line 17 "model.ump"
+// line 196 "model.ump"
+public class Owner extends Staff
+{
+
+  //------------------------
+  // MEMBER VARIABLES
+  //------------------------
+
+  //------------------------
+  // CONSTRUCTOR
+  //------------------------
+
+  public Owner(String aUserName, int aIdNum)
+  {
+    super(aUserName, aIdNum);
+  }
+
+  //------------------------
+  // INTERFACE
+  //------------------------
+
+  public void delete()
+  {
+    super.delete();
+  }
+
+}
