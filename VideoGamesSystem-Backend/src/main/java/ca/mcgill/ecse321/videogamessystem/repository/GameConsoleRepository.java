bed7bbb9054492d91f6edfe2274bd53598b53c43
E10o0 <159184572+E10o0@users.noreply.github.com>
2024-11-08 18:36:12 -0500

    repository updates methods

--- VideoGamesSystem-Backend/src/main/java/ca/mcgill/ecse321/videogamessystem/repository/GameConsoleRepository.java
+++ VideoGamesSystem-Backend/src/main/java/ca/mcgill/ecse321/videogamessystem/repository/GameConsoleRepository.java
@@ -6,5 +6,6 @@ import ca.mcgill.ecse321.videogamessystem.model.GameConsole;
 import ca.mcgill.ecse321.videogamessystem.model.GameConsoleId;
 
 public interface GameConsoleRepository extends CrudRepository<GameConsole, GameConsoleId> {
+    // find the game consol with its id 
     GameConsole findGameConsoleByGameConsoleID(GameConsoleId gameConsoleId);
 }
--- VideoGamesSystem-Backend/src/main/java/ca/mcgill/ecse321/videogamessystem/repository/StaffRepository.java
+++ VideoGamesSystem-Backend/src/main/java/ca/mcgill/ecse321/videogamessystem/repository/StaffRepository.java
@@ -1,9 +1,17 @@
 package ca.mcgill.ecse321.videogamessystem.repository;
 
+import java.util.List;
+
 import org.springframework.data.repository.CrudRepository;
 
 import ca.mcgill.ecse321.videogamessystem.model.Staff;
 
 public interface StaffRepository extends CrudRepository<Staff, Long>{
+    //find staff by id
     public Staff findStaffById(Long id);
+// lis staff that is the owner if admin ==yes or the list of the employees if admin ==no
+        public List<Staff> findStaffByadmin(Boolean admin);
+
+        // add find by username and find by email
+
 }
