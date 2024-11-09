4b2c32ec551cbfcd6c68ba23880b83a457efe1c3
E10o0 <159184572+E10o0@users.noreply.github.com>
2024-11-08 19:25:51 -0500

    updates repository

--- VideoGamesSystem-Backend/src/main/java/ca/mcgill/ecse321/videogamessystem/repository/CustomerRepository.java
+++ VideoGamesSystem-Backend/src/main/java/ca/mcgill/ecse321/videogamessystem/repository/CustomerRepository.java
@@ -7,6 +7,10 @@ import ca.mcgill.ecse321.videogamessystem.model.Customer;
 public interface CustomerRepository extends CrudRepository<Customer, Long>{
     //find the customer with its id
     public Customer findCustomerById(Long id);
+
+    public Customer findCustomerByEmail(String email);
+
+    public Customer findCustomerByuserName(String userName);
 }
 // add a find by usertname and find by email
 // does not make sense to find by phine number or house adress
--- VideoGamesSystem-Backend/src/main/java/ca/mcgill/ecse321/videogamessystem/repository/StaffRepository.java
+++ VideoGamesSystem-Backend/src/main/java/ca/mcgill/ecse321/videogamessystem/repository/StaffRepository.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.data.repository.CrudRepository;
 
+import ca.mcgill.ecse321.videogamessystem.model.Customer;
 import ca.mcgill.ecse321.videogamessystem.model.Staff;
 
 public interface StaffRepository extends CrudRepository<Staff, Long>{
@@ -12,6 +13,9 @@ public interface StaffRepository extends CrudRepository<Staff, Long>{
 // lis staff that is the owner if admin ==yes or the list of the employees if admin ==no
         public List<Staff> findStaffByadmin(Boolean admin);
 
-        // add find by username and find by email
+        public Staff findStaffByEmail(String email);
+
+        public Staff findStaffByuserName(String userName);
+
 
 }
