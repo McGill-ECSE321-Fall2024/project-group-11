889481aa3248ba2ac05cea332dff0e7652c001d0
E10o0 <159184572+E10o0@users.noreply.github.com>
2024-11-30 16:43:42 -0500

    updated the router

--- VideoGamesSystem-Frontend/src/router/index.js
+++ VideoGamesSystem-Frontend/src/router/index.js
@@ -6,6 +6,15 @@ import Cart from "../components/Cart.vue";
 import MyGames from "../components/MyGames.vue";
 import Login from "../components/Login.vue";
 import Signup from "../components/Signup.vue";
+import AddEmployee from "../components/AddEmployee.vue";
+import AddGames from "../components/AddGames.vue";
+import AddPromotions from "../components/AddPromotions.vue";
+import MyAccount from "../components/MyAccount.vue";
+import UpdateUsername from "../components/UpdateUsername.vue";
+import UpdateEmail from "../components/UpdateEmail.vue";
+import UpdatePassword from "../components/UpdatePassword.vue";
+
+
 
 const routes = [
   { path: "/", name: "Home", component: Home },
@@ -14,8 +23,14 @@ const routes = [
   { path: "/cart", name: "Cart", component: Cart },
   { path: "/my-games", name: "MyGames", component: MyGames },
   { path: "/login", name: "Login", component: Login },
-  { path: "/signup", name: "Signup", component: Signup
-   },
+  { path: "/signup", name: "Signup", component: Signup},
+  { path : "AddEmployee", name: "AddEmployee", component: AddEmployee},
+  { path : "AddGames", name: "AddGames", component: AddGames},
+  { path : "AddPromotions", name: "AddPromotions", component: AddPromotions},
+  { path : "MyAccount", name: "MyAccount", component: MyAccount},
+  { path : "UpdateUserName", name: "UpdateUserName", component: UpdateUserName},
+  { path : "UpdateEmail", name: "UpdateEmail", component: UpdateEmail},
+  { path : "UpdatePassword", name: "UpdatePassword", component: UpdatePassword},
 ];
 
 const router = createRouter({
